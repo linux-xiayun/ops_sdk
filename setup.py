bd4f89232abe95dbe9afbab561a38e14a2600148
xiayun <xiayun@startdt.com>
2020-01-13 14:45:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@
 """"
 author : shenshuo
 date   : 2018年2月5日13:37:54
-desc   : SDK
+desc   : OPS SDK
 """
 from distutils.core import setup
 
+__version__ = '0.0.21'
+
 setup(
     name='opssdk',
-    version='0.0.14',
-    packages=['opssdk', 'opssdk.logs', 'opssdk.operate', 'opssdk.install', 'opssdk.get_info', 'opssdk.utils', 'websdk'],
+    version=__version__,
+    packages=['opssdk', 'opssdk.logs', 'opssdk.operate', 'opssdk.install', 'opssdk.get_info', 'opssdk.utils', 'websdk'
+              ,'websdk.apis'],
     url='https://github.com/ss1917/ops_sdk/',
     license='',
-    install_requires=['fire', 'shortuuid', 'pymysql==0.9.3', 'sqlalchemy==1.3.0', 'python3-pika==0.9.14', 'PyJWT',
-                      'Crypto==1.4.1', 'requests', 'redis==2.10.6', 'tornado==5.0',
+    install_requires=['fire==0.1.3', 'shortuuid', 'pymysql==0.9.3', 'sqlalchemy==1.3.0', 'python3-pika==0.9.14', 'PyJWT',
+                      'Crypto==1.4.1', 'requests', 'redis==2.10.6', 'tornado>=5.0',
                       'aliyun-python-sdk-core-v3==2.8.6', 'aliyun-python-sdk-dysmsapi', 'python-dateutil==2.7.5',
                       'ldap3==2.6', 'pycryptodome'],
     author='shenshuo',
